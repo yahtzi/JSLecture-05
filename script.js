f668c2d12f659e96ac30b3604e99f93ceff7eb18
yahtzi <yaritzabejarano7220@my.whccd.com>
2020-01-27 19:34:16 -0800

    I've defined myNumbers moving on to pi

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+//Define a variable "myGreetins" and place a string containing a generic greeting inside of it//
 let myGreeting = "Howdy Globe!";
+
+//Output the contents of "MyGreeting" using console.log(...)//
 console.log(myGreeting);
 
-const pi = 3.1415
+//Define a constant variable "pi" and place a number containing the value fo rpi inside of it//
+const pi = 3.1415;
+
+//Attempt to re-assign "pi" to something else- what happens? You can't reassign a constant//
+pi = 1499; 
+
+//Define a new variable "myNumber" and place a number of your choice inside of it//
+let myNumber = 5592057369;
